d95a6d632a32509fbc3c297fed68d1000f8d809e
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-29 04:03:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,9 +11,20 @@ class Song
     @genre = genre
 
     @@count += 1
+    @@artists
+    @@genres
+
   end
 
   def self.count
     @@count
+  end
+
+  def self.artists
+    @@artists
+  end
+
+  def self.genres
+    @@genres
   end
 end
